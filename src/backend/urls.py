9af93129d610692f6d21d3b58c7de9de11658565
Junhee Lee <jheelee020421@gmail.com>
2025-12-09 20:51:24 -0500

    feat: jwt token

--- src/backend/urls.py
+++ src/backend/urls.py
@@ -18,7 +18,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 def root(request):
     return JsonResponse({"message": "Gloda API"})
@@ -28,4 +31,6 @@ urlpatterns = [
     path("", root),
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
